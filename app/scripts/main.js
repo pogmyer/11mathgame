3af35ace084b27d35aee24999ddfe185fb37424e
pogmyer <pogmyer@yahoo.com>
2023-05-09 01:25:00 -0500

    up to 44:28 of vid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@ const ourForm = document.querySelector(".our-form")
 const ourField = document.querySelector(".our-field")
 const pointsNeeded = document.querySelector(".points-needed")
 const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
 
 let state = {
     score: 0, 
@@ -49,8 +50,38 @@ function handleSubmit(e) {
         state.score++
         pointsNeeded.textContent = 10 - state.score
         updateProblem()
+        renderProgressBar()
     } else {
         state.wrongAnswers++
         mistakesAllowed.textContent = 2 - state.wrongAnswers
     }
+    checkLogic()
+}
+
+function checkLogic() {
+    // if you won
+    if (state.score === 10) {
+        alert("You Won!")
+        resetGame()
+    }
+
+    // if you lost
+    if (state.wrongAnswers === 3) {
+        alert("Oops, You Lost. Try again.")
+        resetGame()
+    }
+
+}
+
+function resetGame() {
+    updateProblem()
+    state.score = 0
+    state.wrongAnswers = 0
+    pointsNeeded.Needed.textContent = 10
+    mistakesAllowed.textContent = 2
+    renderProgressBar()
+}
+
+function renderProgressBar() {
+    progressBar.style.transform = `scaleX(${state.score / 10})`
 }
